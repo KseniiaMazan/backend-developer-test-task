41fab2c0f839d1e0ad1f7ca005786f097778fafc
Ksenia Mazan <kseniamazan@Ksenias-MacBook-Air.local>
2024-04-01 19:02:30 -0700

    added tests for extended developer api

---
+++ test/setup/mockData.ts
@@ -0,0 +1,24 @@
+export const GetDeveloperByIdMockResponse = {
+    "id": "65de346c255f31cb84bd10e9",
+    "email": "Brandon30@hotmail.com",
+    "firstName": "Brandon",
+    "lastName": "D'Amore",
+    "revenue": 12000,
+}
+
+export const GetDevelopersMockResponse = [
+    {
+        "id": "65de346c255f31cb84bd10e9",
+        "email": "Brandon30@hotmail.com",
+        "firstName": "Brandon",
+        "lastName": "D'Amore",
+        "revenue": 12000,
+    },
+    {
+        "id": "65de3467255f31cb84bd07c2",
+		"email": "Makenna.Hand@yahoo.com",
+		"firstName": "Makenna",
+		"lastName": "Hand",
+        "revenue": 0,
+    }
+]
